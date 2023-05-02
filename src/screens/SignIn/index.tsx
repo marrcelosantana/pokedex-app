f717a3d744deb7fa53b3cc3198848e1478d3271d
marrcelosantana <marcelosantana@alu.ufc.br>
2023-05-02 11:32:29 -0300

    [Pokedex-App] - Add social button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,15 @@
+import AppleLogo from "@assets/apple.svg";
+import GoogleLogo from "@assets/google.svg";
+
+import { SocialButton } from "@components/SocialButton";
+
 import { Container, Title } from "./styles";
 
 export function SignIn() {
   return (
     <Container>
-      <Title>Login</Title>
+      <SocialButton title="Sign in with Google" svg={GoogleLogo} />
+      <SocialButton title="Sign in with Apple" svg={AppleLogo} />
     </Container>
   );
 }
